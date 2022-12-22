5fac724da7e8901ec1cd9f65cf6cb94b2efaa704
Kraktu <57345348+Kraktu@users.noreply.github.com>
2022-12-22 16:21:47 +0100

    Creation de personnage, vide

---
+++ proto/Personnage.java
@@ -0,0 +1,24 @@
+package proto;
+
+/**
+ * Personnage créé par Philip
+ */
+public class Personnage {
+    //Properties
+    protected String nom;
+    protected Race race;
+    protected Classe classe;
+    protected int niveau,vie,mana;
+    protected Statistique force,intelligence,agilite,dexterite,constitution;
+
+    //Magic
+    //Constructeur
+    
+    //Getters
+
+    //Setters
+
+    //Override
+
+    
+}
