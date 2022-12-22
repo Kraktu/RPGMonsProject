cb0275e34168e214b8df0125e7fb625042ca0a55
David <dehoustdavid@gmail.com>
2022-12-22 16:00:35 +0100

    correction statistique.java

--- proto/Statistique.java
+++ proto/Statistique.java
@@ -1,57 +1,69 @@
 package proto;
 
 /**
- * Statistique
+ * fait par Simon
  */
 public class Statistique {
 
-private int force;
-private int intelligence;
-private int agilite;
-private int dexterite;
-private int constitution;
-public Animal(int _force, int _intelligence, int _agilite, int _dexterite, int _constitution) {
-    force=_force;
-}intelligence=_intelligence;
-agilite=_agilite;
-dexterite=_dexterite;
-constitution=_constitution
+    private int force;
+    private int intelligence;
+    private int agilite;
+    private int dexterite;
+    private int constitution;
 
-/**
- * getters
- */
-public int GetForce(){
-    return force;
-}
-public int GetIntelligence() {return intelligence;
-    
-}
-public int GetAgilite() {return agilite;
-    
-}
-public int GetDexterite(){
-    return dexterite;
-}
-public int GetConstitution() {
-    return constitution;
-}
+    public Statistique(int _force, int _intelligence, int _agilite, int _dexterite, int _constitution) {
 
-/**
- * setters
- */
-public void SetForce(int _newForce) {
-    force=_newForce;
-}
-public void SetIntelligence(int _newIntelligence) {
-    intelligence=_newIntelligence
-}
-public void SetAgilite(int _newAgilite) {
-    agilite=_newAgilite
-}
-public void SetDexterite(int _newDexterite) {
-    dexterite=_newDexterite;
-    public void SetConstitution(int _newConstitution) {
-        constitution=_newConstitution
+        force = _force;
+        intelligence = _intelligence;
+        agilite = _agilite;
+        dexterite = _dexterite;
+        constitution = _constitution;
     }
-}
+
+    /**
+     * getters
+     */
+    public int GetForce() {
+        return force;
+    }
+
+    public int GetIntelligence() {
+        return intelligence;
+    }
+
+    public int GetAgilite() {
+        return agilite;
+    }
+
+    public int GetDexterite() {
+        return dexterite;
+    }
+
+    public int GetConstitution() {
+        return constitution;
+    }
+
+    /**
+     * setters
+     */
+    public void SetForce(int _newForce) {
+        force = _newForce;
+    }
+
+    public void SetIntelligence(int _newIntelligence) {
+        intelligence = _newIntelligence;
+    }
+
+    public void SetAgilite(int _newAgilite) {
+        agilite = _newAgilite;
+    }
+
+    public void SetDexterite(int _newDexterite) {
+        dexterite = _newDexterite;
+
+    }
+
+    public void SetConstitution(int _newConstitution) {
+        constitution = _newConstitution;
+    }
 }
