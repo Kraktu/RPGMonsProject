279969f6db6c0feaee22e97978e279e8821280f5
MattrimCotton <Msimon.desseille@gmail.com>
2022-12-22 15:52:22 +0100

    modification du fichier statistique.java

--- proto/Statistique.java
+++ proto/Statistique.java
@@ -5,5 +5,53 @@ package proto;
  */
 public class Statistique {
 
+private int force;
+private int intelligence;
+private int agilite;
+private int dexterite;
+private int constitution;
+public Animal(int _force, int _intelligence, int _agilite, int _dexterite, int _constitution) {
+    force=_force;
+}intelligence=_intelligence;
+agilite=_agilite;
+dexterite=_dexterite;
+constitution=_constitution
+
+/**
+ * getters
+ */
+public int GetForce(){
+    return force;
+}
+public int GetIntelligence() {return intelligence;
     
+}
+public int GetAgilite() {return agilite;
+    
+}
+public int GetDexterite(){
+    return dexterite;
+}
+public int GetConstitution() {
+    return constitution;
+}
+
+/**
+ * setters
+ */
+public void SetForce(int _newForce) {
+    force=_newForce;
+}
+public void SetIntelligence(int _newIntelligence) {
+    intelligence=_newIntelligence
+}
+public void SetAgilite(int _newAgilite) {
+    agilite=_newAgilite
+}
+public void SetDexterite(int _newDexterite) {
+    dexterite=_newDexterite;
+    public void SetConstitution(int _newConstitution) {
+        constitution=_newConstitution
+    }
+}
 }
