33737a883ad132af67df9eb77434aec9cf7f6cc6
David <dehoustdavid@gmail.com>
2022-12-22 15:56:14 +0100

    correction de race.java

--- proto/Race.java
+++ proto/Race.java
@@ -1,32 +1,41 @@
 package proto;
-// fait par Seb
+
 /**
- * Race
+ * fait par Seb
  */
+
 public class Race {
-// propriete
-private String nom;
-private Statistique stat;
-// constructeur
-public Race(String nom, Statistique stat ) {
-nom=_nom;
-stat=_stat;    
-}
 
-// getters
-    public String  GetNom() {
-return nom;        
+    // propriete
+
+    private String nom;
+    private Statistique stat;
+
+    // constructeur
+
+    public Race(String _nom, Statistique _stat) {
+        nom = _nom;
+        stat = _stat;
     }
+
+    // getters
+
+    public String GetNom() {
+        return nom;
+    }
+
     public Statistique GetStat() {
-return stat;        
+        return stat;
     }
-// Setters
-public void SetNom(String _nom) {
-    nom=_nom;
-}
-public void SetStat(Statistique _stat) {
-stat=_stat;    
-}
 
-    
+    // Setters
+
+    public void SetNom(String _nom) {
+        nom = _nom;
+    }
+
+    public void SetStat(Statistique _stat) {
+        stat = _stat;
+    }
+
 }
