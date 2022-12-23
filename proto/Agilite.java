a0cbbabac74d5136af8160b3a117e5d8018d5b74
Sebastien Baloge <s.baloge@yahoo.fr>
2022-12-23 14:46:44 +0100

    ajout de intelligence.java et agilite.java

---
+++ proto/Agilite.java
@@ -0,0 +1,7 @@
+import proto.Statistique;
+
+public class Agilite extends Statistique {
+public Agilite(int _valeur) {
+super("Agilité", "Statistique prise en comptes pour les actions de Déplacement et de Vitesse. Permet de déterminer si le joueur est premier à agir en combat, augmente les chances d'esquives des attaques adverses.", _valeur);    
+}    
+}
---
+++ proto/Intelligence.java
@@ -0,0 +1,7 @@
+public class Intelligence extends Statistique {
+
+    public Intelligence(int _valeur) {
+super("Intelligence", "Statistiques prise en compte pour les actions Magique et de Savoir. Augmente les dégats et les soins prodigués par les armes magiques et les sorts Magiques. Augmente aussi la résistance à la Magie.", _valeur);        
+    }
+
+}
