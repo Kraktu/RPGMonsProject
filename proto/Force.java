df78b5b2de8c796379cb2d7019ba6fbf9013ad57
David <dehoustdavid@gmail.com>
2022-12-23 14:24:35 +0100

    creation de la statistique force

---
+++ proto/Force.java
@@ -0,0 +1,13 @@
+package proto;
+
+/**
+ * Force par David
+ */
+
+public class Force extends Statistique {
+
+    public Force (int _valeur){
+        super("Force", "Statistiques prise en compte pour les actions physique, et augmente les dégats fait au corps à corps avec presque toutes les armes non magique.", _valeur);
+    }
+
+}
