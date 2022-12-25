9c74851d9e134dfe79241625e5f84333dac2aa05
BrunoDefalque <defalquebruno@gmail.com>
2022-12-25 17:24:32 +0100

    Ajout Override toString et equals dans Statistique.java

--- proto/Statistique.java
+++ proto/Statistique.java
@@ -5,45 +5,84 @@ package proto;
  */
 public class Statistique {
 
-    private String nom;
-    private String description;
-    private int valeur;
+	private String nom;
+	private String description;
+	private int valeur;
 
-    public Statistique (String _nom, String _description, int _valeur) {
+	public Statistique(String _nom, String _description, int _valeur) {
 
-        nom = _nom;
-        description = _description;
-        valeur = _valeur;
-    }
+		nom = _nom;
+		description = _description;
+		valeur = _valeur;
+	}
 
-    /**
-     * getters
-     */
-    public String GetNom() {
-        return nom;
-    }
+	/**
+	 * getters
+	 */
+	public String GetNom() {
+		return nom;
+	}
 
-    public String GetDescription() {
-        return description;
-    }
+	public String GetDescription() {
+		return description;
+	}
 
-    public int GetValeur() {
-        return valeur;
-    }
+	public int GetValeur() {
+		return valeur;
+	}
 
-    /**
-     * setters
-     */
-    public void SetNom(String _nom) {
-        nom = _nom;
-    }
+	/**
+	 * setters
+	 */
+	public void SetNom(String _nom) {
+		nom = _nom;
+	}
 
-    public void SetDescription(String _description) {
-        description = _description;
-    }
+	public void SetDescription(String _description) {
+		description = _description;
+	}
 
-    public void SetValeur(int _valeur) {
-        valeur = _valeur;
-    }
+	public void SetValeur(int _valeur) {
+		valeur = _valeur;
+	}
 
+	// Override
+
+	@Override
+
+	public String toString() {
+
+		String returnString = "Nom : " + nom +
+
+				"\nDéscription : " + description +
+
+				"\nValeur statistique : " + valeur;
+
+		return returnString;
+	}
+
+	@Override
+
+	public boolean equals(Object _o) {
+
+		if (_o == null) {
+
+			return false;
+		}
+
+		if (_o.getClass() != getClass()) {
+
+			return false;
+
+		}
+
+		Statistique _statistique = (Statistique) _o;
+
+		if (nom.equals(_statistique.nom) && description.equals(_statistique.description) && valeur == _statistique.valeur) {
+
+			return true;
+		}
+
+		return false;
+	}
 }
