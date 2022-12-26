709bf1d908235b4eba39fff61357fbc046b646a9
MattrimCotton <Msimon.desseille@gmail.com>
2022-12-26 11:26:12 +0100

    Création de Guerrier.java

---
+++ proto/Guerrier.java
@@ -0,0 +1,11 @@
+package proto;
+
+/**
+ * Guerrier
+ */
+public class Guerrier extends Classe {
+
+    public Guerrier (String _nom, String _description){
+        super("Guerrier", "Le barbare est formé au maniement de toutes les armes courantes et de guerre, ainsi qu’au port des armures légères et intermédiaires, et à l’utilisation des boucliers (à l’exception des pavois).");
+    }
+}
