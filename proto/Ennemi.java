8eee2b57cfd644627009a6a1feecef70c18ac8d3
MattrimCotton <Msimon.desseille@gmail.com>
2022-12-26 09:54:23 +0100

    Correction du fichier Ennemi.java

--- proto/Ennemi.java
+++ proto/Ennemi.java
@@ -1,12 +1,13 @@
 
 /**
- * Ennemi
+* Créé par Simon 
+* Ennemi
  */
 public class Ennemi extends Personnage {
     private Arme arme;
     private Classe classe;
-    private Equipement Equipement;
-public Ennemi (Arme _arme, Classe _classe, Equipement _equipement) {
+    private Equipement equipement;
+public Ennemi (String _nom, Race _race, Classe _classe, int _niveau, int _vie, int _mana, Statistique _force, Statistique _intelligence, Statistique _agilite, Statistique _dexterite, Statistique _constitution, Arme _arme, Equipement _equipement) {
     super(_nom, _race, _classe, _niveau, _vie, _mana, _force, _intelligence, _agilite, _dexterite, _constitution);
     arme=_arme;
     classe=_classe;
