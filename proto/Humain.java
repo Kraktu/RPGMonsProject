e03a38ae2ba07d14b42e93b12d81f8fc6f134817
Sebastien Baloge <s.baloge@yahoo.fr>
2022-12-27 14:56:03 +0100

    modf humain.java

--- proto/Humain.java
+++ proto/Humain.java
@@ -3,11 +3,48 @@ package proto;
  * Humain, créer par Seb
  */
 public class Humain extends Race {
+// Proprietes
+    private String nom;
+    private String description;
+    private ForceBonus forceBonus;
 
-    public Humain(String _nom, Force _forceBonus, Intelligence _intelBonus, Agilite _agilBonus) {
+    // constructeur
 
-        super("Humain", new Force(5), new Intelligence(5), new Agilite(5));
-
+    public Humain(String _nom, Agilite _agilite, Constitution _constitution, Dexterite _dexterite, Force _force, Intelligence _intelligence, String nom, String description, ForceBonus forceBonus) {
+        super("Humain", 5, 5, 5, 5, 5);
+        nom = _nom;
+        description = _description;
+        forceBonus = _forceBonus;
     }
 
+
+// Getters
+
+
+    public String GetNom() {
+        return nom;
+    }
+
+    public String GetDescription() {
+        return description;
+    }
+
+    public ForceBonus GetForceBonus() {
+        return forceBonus;
+    }
+
+    // Setters
+
+
+    public void SetNom(String _nom) {
+        nom = _nom;
+    }
+
+    public void SetDescription(String _description) {
+        description = _description;
+    }
+
+    public void SetForceBonus(ForceBonus _forceBonus) {
+        forceBonus = _forceBonus;
+    }
 }
