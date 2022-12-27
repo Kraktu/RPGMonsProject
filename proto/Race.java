0886a8edd0af8028f330e2f7752f56f6d50cbc4a
Sebastien Baloge <s.baloge@yahoo.fr>
2022-12-27 14:11:29 +0100

    modification de race.java

--- proto/Race.java
+++ proto/Race.java
@@ -7,35 +7,72 @@ package proto;
 public class Race {
 
     // propriete
-
     private String nom;
-    private Statistique stat;
-
+    private Agilite agilite;
+private Constitution constitution;
+    private Dexterite dexterite;
+    private Force force;
+    private Intelligence intelligence;
     // constructeur
-
-        public Race(String _nom, Statistique _stat) {
+    public Race(String nom, Agilite agilite, Constitution constitution, Dexterite dexterite, Force force, Intelligence intelligence) {
         nom = _nom;
-        stat = _stat;
+        agilite = _agilite;
+        constitution = _constitution;
+        dexterite = _dexterite;
+        force = _force;
+        intelligence = _intelligence;
     }
+// Getters
 
-    // getters
 
     public String GetNom() {
         return nom;
     }
 
-    public Statistique GetStat() {
-        return stat;
+    public Agilite GetAgilite() {
+        return agilite;
+    }
+
+    public Constitution GetConstitution() {
+        return constitution;
+    }
+
+    public Dexterite GetDexterite() {
+        return dexterite;
+    }
+
+    public Force GetForce() {
+        return force;
+    }
+
+    public Intelligence GetIntelligence() {
+        return intelligence;
     }
 
     // Setters
+
 
     public void SetNom(String _nom) {
         nom = _nom;
     }
 
-    public void SetStat(Statistique _stat) {
-        stat = _stat;
+    public void SetAgilite(Agilite _agilite) {
+        agilite = _agilite;
     }
 
+    public void SetConstitution(Constitution _constitution) {
+        constitution = _constitution;
+    }
+
+    public void SetDexterite(Dexterite _dexterite) {
+        dexterite = _dexterite;
+    }
+
+    public void SetForce(Force _force) {
+        force = _force;
+    }
+
+    public void SetIntelligence(Intelligence _intelligence) {
+        intelligence = _intelligence;
+    }
 }
