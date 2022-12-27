15891195207ec110e62077149120ec52fd095c40
David Dehoust <dehoustdavid@gmail.com>
2022-12-27 13:51:45 +0100

    ajout de ForceBonus.java

---
+++ proto/ForceBonus.java
@@ -0,0 +1,15 @@
+package proto;
+
+/**
+ * ForceBonus par David
+ */
+
+public class ForceBonus extends Statistique {
+
+    public ForceBonus(int _valeur) {
+
+        super ("Force bonus", "Bonus de force lié à la race.", _valeur);
+
+    }
+
+}
