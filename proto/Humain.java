d488ba292ffe0b5aba3efff9ac7f4f865c74d2ea
David Dehoust <dehoustdavid@gmail.com>
2022-12-29 09:16:40 +0100

    reuplaud du main.java + correction humain.java

--- proto/Humain.java
+++ proto/Humain.java
@@ -1,4 +1,5 @@
 package proto;
+
 /**
  * Humain, créer par Seb
  */
@@ -8,6 +9,6 @@ public class Humain extends Race {
     // Constructeur
 
     public Humain(String _nom, String _description, StatistiqueBonus _forceBonus, StatistiqueBonus _intelligenceBonus, StatistiqueBonus _agiliteBonus, StatistiqueBonus _constitutionBonus, StatistiqueBonus _dexteriteBonus) {
-        super("Humain", "C'est un humain", 5, 0, 0, 0, 0);
+        super("Humain", "C'est un humain", new StatistiqueBonus(5), new StatistiqueBonus(5), new StatistiqueBonus(5), new StatistiqueBonus(5), new StatistiqueBonus(5));
     }
 }
---
+++ proto/Main.java
@@ -0,0 +1,35 @@
+package proto;
+/**
+ * main, créé par David
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Création du joueur.
+        // tout remettre sur une ligne a la fin
+        Joueur joueur = new Joueur(
+                "le nouveau",
+                null,
+                null,
+                5,
+                100,
+                100,
+                new Force(5),
+                new Intelligence(5),
+                new Agilite(5),
+                new Dexterite(5),
+                new Constitution(5),
+                Arsenal.GetInstance().epee,
+                50,
+                50);
+
+        // print d'un toString
+        System.out.println(Arsenal.GetInstance().epee.toString());
+
+        // Le print de joueur est assez envahissant, faut que je jette un oeil à ca.
+        // System.out.println(joueur.toString());
+
+    }
+
+}
