9421ddc6b3d5d44341ae983cf11c74c9e024e8b6
Sebastien Baloge <s.baloge@yahoo.fr>
2022-12-29 09:52:09 +0100

    corrections

--- proto/Gobelin.java
+++ proto/Gobelin.java
@@ -4,7 +4,7 @@ package proto;
  */
 public class Gobelin extends Race{
     public Gobelin(String _nom, String _description, StatistiqueBonus _forceBonus, StatistiqueBonus _intelligenceBonus, StatistiqueBonus _agiliteBonus, StatistiqueBonus _constitutionBonus, StatistiqueBonus _dexteriteBonus) {
-        super("Gobelin", "C'est un gobelin", 0, 0, 0, 0, 5);
+        super("Gobelin", "C'est un gobelin", new StatistiqueBonus(0), new StatistiqueBonus(0), new StatistiqueBonus(0), new StatistiqueBonus(0), new StatistiqueBonus(5));
     }
 
 }
--- proto/Troll.java
+++ proto/Troll.java
@@ -5,7 +5,7 @@ package proto;
 public class Troll extends Race{
     // Constructeur
     public Troll(String _nom, String _description, StatistiqueBonus _forceBonus, StatistiqueBonus _intelligenceBonus, StatistiqueBonus _agiliteBonus, StatistiqueBonus _constitutionBonus, StatistiqueBonus _dexteriteBonus) {
-        super("Troll", "C'est un troll", 0, 0, 0, 5, 0);
+        super("Troll", "C'est un troll", new StatistiqueBonus(0), new StatistiqueBonus(0), new StatistiqueBonus(0), new StatistiqueBonus(5), new StatistiqueBonus(0));
     }
 
 }
