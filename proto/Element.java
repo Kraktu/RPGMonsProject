e5662b1b46e91976dfdef331a6ed63e44c8e7f71
BrunoDefalque <defalquebruno@gmail.com>
2023-01-01 22:22:29 +0100

    Ajout fichier Element.java + class

---
+++ proto/Element.java
@@ -0,0 +1,11 @@
+package proto;
+
+/*Element créé par Bruno
+ */
+public class Element {
+
+    /*
+    string name;
+    Element beatenElement, loosingElement;
+    int bonusAgainstBeatenElement, malusAgainstLoosingElement;*/
+}
