c2fe5b475b6371d4ac6fa5f1f053e00cb2e86cf5
Sebastien Baloge <s.baloge@yahoo.fr>
2023-01-02 10:13:06 +0100

    correction de ennemi.java

--- proto/Ennemi.java
+++ proto/Ennemi.java
@@ -7,7 +7,7 @@ public class Ennemi extends Personnage {
     private Arme arme;
     private Equipement equipement;
 public Ennemi (String _nom, Race _race, Classe _classe, int _niveau, int _vie, int _mana, Statistique _force, Statistique _intelligence, Statistique _agilite, Statistique _dexterite, Statistique _constitution, Arme _arme, Equipement _equipement) {
-    super(_nom, _race, _niveau, _vie, _mana, _force, _intelligence, _agilite, _dexterite, _constitution);
+    super(_nom, _race, _classe, _niveau, _vie, _mana, _force, _intelligence, _agilite, _dexterite, _constitution);
     arme=_arme;
     equipement=_equipement;
 }
