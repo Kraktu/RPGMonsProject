cebf7cfa8bdc6bc7d59eb26823e87fbbaf2b9ef2
Sebastien Baloge <s.baloge@yahoo.fr>
2023-01-02 11:18:32 +0100

    ajout de la fonction ChoixClasse

--- proto/EntreUtilisateur.java
+++ proto/EntreUtilisateur.java
@@ -19,4 +19,28 @@ public class EntreUtilisateur {
         return reponse;
     }
 
+public int ChoixClasse () {
+        int choix=EntreeUtilisateur("Choisissez votre classe [tapez 1" pour guerrier, \n2" pour mage, \n3" pour voleur, \n4" pour archer")) {
+switch (choix) {
+    case "1" :
+        System.out.println("Vous avez choisi guerrier.");
+        break;
+    case "2" :
+        System.out.println("Vous avez choisi mage.");
+        break;
+    case "3" :
+        System.out.println("Vous avez choisi voleur.");
+        break;
+    case "4" :
+        System.out.println("Vous avez choisi archer.");
+        break;
+    default:
+        System.out.println("Erreur, veuillez choisir un nombre compris entre 1 et 4.");
+        ChoixClasse();
+        break;
+}
+return choix;
+
+
+}
 }
