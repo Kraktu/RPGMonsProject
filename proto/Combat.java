86d7348df02ff267ded2c8f0fadf936f16659478
Sebastien Baloge <s.baloge@yahoo.fr>
2023-01-04 15:51:18 +0100

    amélioration de combat

--- proto/Combat.java
+++ proto/Combat.java
@@ -4,16 +4,25 @@ package proto;
  * Combat crée par Seb
  */
 public class Combat {
-    public void SeBattre() {
+public int formuleCombat (int _force, int _constitution) {
+int _pointDeVie=_constitution-_force;
+return _pointDeVie;
+}
+   public void SeBattre() {
         System.out.println("Le dé est lancé pour déterminer celui qui va engager le combat.");
         NbrAleatoire nbrAl = new NbrAleatoire();
         int jet = nbrAl.GenererNbrAleatoireBorne(1, 6);
         System.out.println("Le chiffre obtenu est " + jet);
         if (jet < 3) {
             System.out.println("C'est à vous de comencer.");
+            System.out.println("Vous attaquez votre ennemi.\nSes points de vie restants sont " +             formuleCombat(9, 2));
+            
         } else {
             System.out.println("Votre ennemi commence.");
+            System.out.println("Il vous attaque. Vos points de vie restant sont " +             formuleCombat(9, 2));
+            
         }
+
     }
 
 }
