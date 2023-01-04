19b6f00b7d43b1f8de29fed55ac8d8308b51570f
Sebastien Baloge <s.baloge@yahoo.fr>
2023-01-04 15:05:44 +0100

    correction de combat

--- proto/Combat.java
+++ proto/Combat.java
@@ -1,24 +1,19 @@
-import proto.NbrAleatoire;
+package proto;
 
 /**
  * Combat crée par Seb
  */
 public class Combat {
-public void SeBattre() {
-    System.out.println("Le dé est lancé pour déterminer celui qui va engager le combat.");
-NbrAleatoire nbrAl = new NbrAleatoire();
-int jet = nbrAl.GenererNbrAleatoireBorne(1, 6);
-System.out.println("Le chiffre obtenu est " + jet);
-if (jet<3) {
-System.out.println("C'est à vous de comencer.");    
-}
-else if (jet>3) {
-System.out.println("Votre ennemi commence.");    
-}
-else {
-System.out.println("égalité !");
-SeBattre();
-}
-}
-    
+    public void SeBattre() {
+        System.out.println("Le dé est lancé pour déterminer celui qui va engager le combat.");
+        NbrAleatoire nbrAl = new NbrAleatoire();
+        int jet = nbrAl.GenererNbrAleatoireBorne(1, 6);
+        System.out.println("Le chiffre obtenu est " + jet);
+        if (jet < 3) {
+            System.out.println("C'est à vous de comencer.");
+        } else {
+            System.out.println("Votre ennemi commence.");
+        }
+    }
+
 }
