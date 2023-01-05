757033d6a47e50c703a8d351a7a95398b184dab4
Sebastien Baloge <s.baloge@yahoo.fr>
2023-01-05 09:32:21 +0100

    ajout de la fonction JetDeDe dans combat.java

--- proto/Combat.java
+++ proto/Combat.java
@@ -4,25 +4,43 @@ package proto;
  * Combat crée par Seb
  */
 public class Combat {
-public int formuleCombat (int _force, int _constitution) {
-int _pointDeVie=_constitution-_force;
-return _pointDeVie;
-}
-   public void SeBattre() {
-        System.out.println("Le dé est lancé pour déterminer celui qui va engager le combat.");
+    public int JetDeDe() {
         NbrAleatoire nbrAl = new NbrAleatoire();
-        int jet = nbrAl.GenererNbrAleatoireBorne(1, 6);
-        System.out.println("Le chiffre obtenu est " + jet);
-        if (jet < 3) {
-            System.out.println("C'est à vous de comencer.");
-            System.out.println("Vous attaquez votre ennemi.\nSes points de vie restants sont " +             formuleCombat(9, 2));
-            
-        } else {
-            System.out.println("Votre ennemi commence.");
-            System.out.println("Il vous attaque. Vos points de vie restant sont " +             formuleCombat(9, 2));
-            
+        try {
+            System.out.println("Le dé est lancé.");
+            Thread.sleep(1000);
+            int _jet = nbrAl.GenererNbrAleatoireBorne(1, 6);
+        } catch (Exception e) {
+            // TODO: handle exception
+            System.out.println("Erreur.");
         }
-
+        return _jet;
     }
 
+    public int formuleCombat(int _pointDeVie, int _force, int _constitution) {
+        int _pointDeVieRestant = _pointDeVie - (_constitution - _force);
+        return _pointDeVieRestant;
+    }
+
+    public void SeBattre() {
+
+            int _votreJet = JetDeDe();
+            System.out.println("Vous avez obtenu " + _votreJet + ".");
+            int _jetDeVotreAdversaire = JetDeDe();
+            System.out.println("Votre adversaire a obtenu " + _jetDeVotreAdversaire + ".");
+            if (_votreJet > _jetDeVotreAdversaire) {
+                System.out.println(
+                        "Vous attaquez votre adversaire.\nSes points de vie restants sont " + formuleCombat(50, 6, 5));
+
+            } else if (_jetDeVotreAdversaire > _votreJet) {
+
+                System.out.println(
+                        "Votre adversaire vous attaque. Vos points de vie restant sont " + formuleCombat(50, 9, 2));
+
+            } else {
+                System.out.println("égalité.");
+                SeBattre();
+            }
+        }
+    
 }
