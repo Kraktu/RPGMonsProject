e6cc7a9021aeed5dd4a0b31b6ea182cdceb0190f
David <dehoustdavid@gmail.com>
2023-01-06 10:37:39 +0100

    ajout gestionExperience.java

---
+++ proto/Fonctions/GestionExperience.java
@@ -0,0 +1,13 @@
+package proto.Fonctions;
+
+/**
+ * GainExperience, créer par Simon
+ */
+public class GestionExperience {
+
+    // fonction gainExperience.
+    // entrée : un montant d'experience aquis après un évènement en int.
+    // instruction : le calcul du nouveau montant total d'experience.
+    // sortie : le montant final d'experience en int.
+
+}
