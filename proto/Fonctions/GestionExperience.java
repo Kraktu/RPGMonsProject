55df944e81645642c427361b6d5341ae2bdb0c84
MattrimCotton <Msimon.desseille@gmail.com>
2023-01-06 11:37:39 +0100

    completion du fichier GestionExperience.java

--- proto/Fonctions/GestionExperience.java
+++ proto/Fonctions/GestionExperience.java
@@ -1,13 +1,16 @@
 package proto.Fonctions;
 
 /**
- * GainExperience, créer par Simon
+ * GainExperience, créé par Simon
  */
 public class GestionExperience {
 
     // fonction gainExperience.
-    // entrée : un montant d'experience aquis après un évènement en int.
+    // entrée : un montant d'experience acquis après un évènement en int.
     // instruction : le calcul du nouveau montant total d'experience.
     // sortie : le montant final d'experience en int.
+    public int GainExperience(int _xpActuelle, int _xpMontante) {
+        return _xpActuelle+_xpMontante;
+    }
 
 }
