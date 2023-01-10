32eab7bde399331b526243ac4d0a5797ef3719a9
Sebastien Baloge <s.baloge@yahoo.fr>
2023-01-10 20:25:20 +0100

    ajout des set des points de vie

--- proto/CombatManageur.java
+++ proto/CombatManageur.java
@@ -16,7 +16,6 @@ public class CombatManageur {
     final lesPersonnagesEnCombat perso = new lesPersonnagesEnCombat();
     final MenuCombat _MenuCombat = new MenuCombat();
 
-
     public void SeBattre(lesPersonnagesEnCombat _lesPerso) {
 
         int _pointDeVieRestantDuJoueur = 0;
@@ -27,52 +26,61 @@ public class CombatManageur {
 
         boolean leJoueurCommence = QuiCommence();
 
-
-        //System.out.println(_lesPerso.GetLePersonnageJouable().GetNom());
-
-
         do {
 
             if (leJoueurCommence) {
                 // Instruction pour le cas où le joueur commence.
                 _pointDeVieRestantDeEnnemi = ennemi.GetVie() - FormuleDegat(joueur, ennemi);
-                System.out.println("Vous attaquez votre adversaire.\n Il lui reste " + _pointDeVieRestantDeEnnemi + ".");
-
+                System.out
+                        .println("Vous attaquez votre adversaire.\n Il lui reste " + _pointDeVieRestantDeEnnemi + ".");
+                ennemi.SetVie(_pointDeVieRestantDeEnnemi);
                 _pointDeVieRestantDuJoueur = joueur.GetVie() - FormuleDegat(ennemi, joueur);
-                System.out.println("C'est au tour de votre ennemi de vous attaquer.\nIl vous reste " + _pointDeVieRestantDuJoueur);
-
+                System.out.println(
+                        "C'est au tour de votre ennemi de vous attaquer.\nIl vous reste " + _pointDeVieRestantDuJoueur);
+                joueur.SetVie(_pointDeVieRestantDuJoueur);
             } else {
                 // Instruction pour le cas où l'ennemi commence.
                 _pointDeVieRestantDuJoueur = joueur.GetVie() - FormuleDegat(ennemi, joueur);
-                System.out.println("C'est au tour de votre ennemi de vous attaquer.\nIl vous reste " + _pointDeVieRestantDuJoueur);
-
+                System.out.println(
+                        "C'est au tour de votre ennemi de vous attaquer.\nIl vous reste " + _pointDeVieRestantDuJoueur);
+                joueur.SetVie(_pointDeVieRestantDuJoueur);
                 _pointDeVieRestantDeEnnemi = ennemi.GetVie() - FormuleDegat(joueur, ennemi);
-                System.out.println("Vous attaquez votre adversaire.\n Il lui reste " + _pointDeVieRestantDeEnnemi + ".");
-
+                System.out
+                        .println("Vous attaquez votre adversaire.\n Il lui reste " + _pointDeVieRestantDeEnnemi + ".");
+                ennemi.SetVie(_pointDeVieRestantDeEnnemi);
             }
 
         } while (_pointDeVieRestantDeEnnemi > 0 && _pointDeVieRestantDuJoueur > 0);
 
-
-/*
-        do {
-
-            if (leJoueurCommence) {
-                // Instruction pour le cas où le joueur commence.
-                _pointDeVieRestantDeEnnemi = perso.GetEnnemisActuel().GetVie() - FormuleDegatPourEnnemi();
-                System.out.println("Vous attaquez votre adversaire.\n Il lui reste " + _pointDeVieRestantDeEnnemi + ".");
-                _pointDeVieRestantDuJoueur = perso.GetLePersonnageJouable().GetVie() - FormuleDegatPourJoueur();
-                System.out.println("C'est au tour de votre ennemi de vous attaquer.\nIl vous rest " + _pointDeVieRestantDuJoueur);
-            } else {
-                // Instruction pour le cas où l'ennemi commence.
-                _pointDeVieRestantDuJoueur = perso.GetLePersonnageJouable().GetVie() - FormuleDegatPourJoueur();
-                System.out.println("Votre ennemi vous attaque.\nIl vous rest " + _pointDeVieRestantDuJoueur);
-                _pointDeVieRestantDeEnnemi = perso.GetEnnemisActuel().GetVie() - FormuleDegatPourEnnemi();
-                System.out.println("C'est à votre tour d'attaquer votre adversaire.\n Il lui reste " + _pointDeVieRestantDeEnnemi + ".");
-            }
-
-        } while (_pointDeVieRestantDeEnnemi > 0 || _pointDeVieRestantDuJoueur > 0);
-*/
+        /*
+         * do {
+         * 
+         * if (leJoueurCommence) {
+         * // Instruction pour le cas où le joueur commence.
+         * _pointDeVieRestantDeEnnemi = perso.GetEnnemisActuel().GetVie() -
+         * FormuleDegatPourEnnemi();
+         * System.out.println("Vous attaquez votre adversaire.\n Il lui reste " +
+         * _pointDeVieRestantDeEnnemi + ".");
+         * _pointDeVieRestantDuJoueur = perso.GetLePersonnageJouable().GetVie() -
+         * FormuleDegatPourJoueur();
+         * System.out.
+         * println("C'est au tour de votre ennemi de vous attaquer.\nIl vous rest " +
+         * _pointDeVieRestantDuJoueur);
+         * } else {
+         * // Instruction pour le cas où l'ennemi commence.
+         * _pointDeVieRestantDuJoueur = perso.GetLePersonnageJouable().GetVie() -
+         * FormuleDegatPourJoueur();
+         * System.out.println("Votre ennemi vous attaque.\nIl vous rest " +
+         * _pointDeVieRestantDuJoueur);
+         * _pointDeVieRestantDeEnnemi = perso.GetEnnemisActuel().GetVie() -
+         * FormuleDegatPourEnnemi();
+         * System.out.
+         * println("C'est à votre tour d'attaquer votre adversaire.\n Il lui reste " +
+         * _pointDeVieRestantDeEnnemi + ".");
+         * }
+         * 
+         * } while (_pointDeVieRestantDeEnnemi > 0 || _pointDeVieRestantDuJoueur > 0);
+         */
     }
 
     public int JetDeDe() {
@@ -89,14 +97,16 @@ public class CombatManageur {
     }
 
     public int FormuleDegatPourEnnemi() {
-        return perso.GetLePersonnageJouable().GetForce().GetValeur() - perso.GetEnnemisActuel().GetConstitution().GetValeur();
+        return perso.GetLePersonnageJouable().GetForce().GetValeur()
+                    - perso.GetEnnemisActuel().GetConstitution().GetValeur();
     }
 
     public int FormuleDegatPourJoueur() {
-        return perso.GetEnnemisActuel().GetForce().GetValeur() - perso.GetLePersonnageJouable().GetConstitution().GetValeur();
+        return perso.GetEnnemisActuel().GetForce().GetValeur()
+                - perso.GetLePersonnageJouable().GetConstitution().GetValeur();
     }
 
-    public int FormuleDegat(Personnage _attaquant, Personnage _defenseur){
+    public int FormuleDegat(Personnage _attaquant, Personnage _defenseur) {
         return _attaquant.GetForce().GetValeur() - _defenseur.GetConstitution().GetValeur();
     }
 
