8084d4ddd750d156d20a85923190e0d3e300c833
Sebastien Baloge <s.baloge@yahoo.fr>
2023-01-10 14:15:12 +0100

    ajout de la fonction formuledegatourjoueur et ajout fonction formuledegatpourennemi

--- proto/Combat.java
+++ proto/Combat.java
@@ -7,6 +7,8 @@ import proto.Stat.*;
  * Combat crée par Seb
  */
 public class Combat {
+    lesPersonnagesEnCombat perso = new lesPersonnagesEnCombat();
+
     public int JetDeDe() {
         int _jet = 0;
         NbrAleatoire nbrAl = new NbrAleatoire();
@@ -20,44 +22,55 @@ public class Combat {
         }
         return _jet;
     }
-public int FormuleDegat(Force _force, Constitution _constitution)  {
-return _force.GetValeur()-_constitution.GetValeur();
 
-}
+    public int FormuleDegatPourEnnemi() {
+        return perso.GetLePersonnageJouable().GetForce().GetValeur()
+                - perso.GetEnnemisActuel().GetConstitution().GetValeur();
+
+    }
+
+    public int FormuleDegatPourJoueur() {
+        return perso.GetEnnemisActuel().GetForce().GetValeur()
+                - perso.GetLePersonnageJouable().GetConstitution().GetValeur();
+    }
+
     public void QuiCommence() {
         int _votreJet = JetDeDe();
         System.out.println("Vous avez obtenu " + _votreJet + ".");
         int _jetDeVotreAdversaire = JetDeDe();
         System.out.println("Votre adversaire a obtenu " + _jetDeVotreAdversaire + ".");
         if (_votreJet > _jetDeVotreAdversaire) {
-        System.out.println("C'est à vous de commencer.");
-        }
-        else if (_jetDeVotreAdversaire > _votreJet) {
-System.out.println("Votre adversaire commence.");
-        }
-        else {
-        System.out.println("égalité.");
-        QuiCommence();
+            System.out.println("C'est à vous de commencer.");
+        } else if (_jetDeVotreAdversaire > _votreJet) {
+            System.out.println("Votre adversaire commence.");
+        } else {
+            System.out.println("égalité.");
+            QuiCommence();
         }
     }
 
- /*         public void SeBattre() {
-        int _pointDeVieRestant=0;
-    MenuCombat _MenuCombat = new MenuCombat();
-       do  {
-            
-        
-                _pointDeVieRestant=FormuleDegat(6, 5);
-
-            System.out.println("Vous attaquez votre adversaire.\nSes points de vie restants sont " + _pointDeVieRestant );
-                    _MenuCombat.ChoixMenuCombat();
-
-            _pointDeVieRestant=formuleCombat(50, 9, 2);
-            System.out.println(
-                    "Votre adversaire vous attaque. Vos points de vie restant sont " + _pointDeVieRestant);
-                    _MenuCombat.ChoixMenuCombat();
-
-        
-    } while (_pointDeVieRestant>0);
-    }. */
+    /*
+     * public void SeBattre() {
+     * int _pointDeVieRestant=0;
+     * MenuCombat _MenuCombat = new MenuCombat();
+     * do {
+     * 
+     * 
+     * _pointDeVieRestant=FormuleDegat(6, 5);
+     * 
+     * System.out.
+     * println("Vous attaquez votre adversaire.\nSes points de vie restants sont " +
+     * _pointDeVieRestant );
+     * _MenuCombat.ChoixMenuCombat();
+     * 
+     * _pointDeVieRestant=formuleCombat(50, 9, 2);
+     * System.out.println(
+     * "Votre adversaire vous attaque. Vos points de vie restant sont " +
+     * _pointDeVieRestant);
+     * _MenuCombat.ChoixMenuCombat();
+     * 
+     * 
+     * } while (_pointDeVieRestant>0);
+     * }.
+     */
 }
