559a16f1d600199c4b20a277cd535c9b220a94e0
Sebastien Baloge <s.baloge@yahoo.fr>
2023-01-10 15:28:59 +0100

    modif combat

--- proto/Combat.java
+++ proto/Combat.java
@@ -49,29 +49,29 @@ public class Combat {
     }
 
     public void SeBattre() {
-     
-      MenuCombat _MenuCombat = new MenuCombat();
-int _pointDeVieRestantDuJoueur=0;
-int _pointDeVieRestantDeEnnemi=0;      
-QuiCommence();
-      do {
-      if (condition) {
-_pointDeVieRestantDeEnnemi = perso.GetEnnemisActuel().GetVie()-FormuleDegatPourEnnemi();
-        System.out.println("Vous attaquez votre adversaire.\n Il lui reste " + _pointDeVieRestantDeEnnemi + ".");
-        _pointDeVieRestantDuJoueur = perso.GetLePersonnageJouable().GetVie()-FormuleDegatPourJoueur();
-        System.out.println("C'est autour de votre ennemi de vous attaquer.\nIl vous rest " + _pointDeVieRestantDuJoueur);       
-      }
-      else if() {
-        _pointDeVieRestantDuJoueur = perso.GetLePersonnageJouable().GetVie()-FormuleDegatPourJoueur();
-        System.out.println("Votre ennemi vous attaque.\nIl vous rest " + _pointDeVieRestantDuJoueur);       
-        _pointDeVieRestantDeEnnemi = perso.GetEnnemisActuel().GetVie()-FormuleDegatPourEnnemi();
-        System.out.println("C'est à votre tour d'attaquer votre adversaire.\n Il lui reste " + _pointDeVieRestantDeEnnemi + ".");
 
-      }
+        MenuCombat _MenuCombat = new MenuCombat();
+        int _pointDeVieRestantDuJoueur = 0;
+        int _pointDeVieRestantDeEnnemi = 0;
+        QuiCommence();
+        do {
+            if (true) {
+                _pointDeVieRestantDeEnnemi = perso.GetEnnemisActuel().GetVie() - FormuleDegatPourEnnemi();
+                System.out
+                        .println("Vous attaquez votre adversaire.\n Il lui reste " + _pointDeVieRestantDeEnnemi + ".");
+                _pointDeVieRestantDuJoueur = perso.GetLePersonnageJouable().GetVie() - FormuleDegatPourJoueur();
+                System.out.println(
+                        "C'est autour de votre ennemi de vous attaquer.\nIl vous rest " + _pointDeVieRestantDuJoueur);
+            } else if (false) {
+                _pointDeVieRestantDuJoueur = perso.GetLePersonnageJouable().GetVie() - FormuleDegatPourJoueur();
+                System.out.println("Votre ennemi vous attaque.\nIl vous rest " + _pointDeVieRestantDuJoueur);
+                _pointDeVieRestantDeEnnemi = perso.GetEnnemisActuel().GetVie() - FormuleDegatPourEnnemi();
+                System.out.println("C'est à votre tour d'attaquer votre adversaire.\n Il lui reste "
+                        + _pointDeVieRestantDeEnnemi + ".");
 
+            }
 
-     
-      } while (_pointDeVieRestantDeEnnemi>0 || _pointDeVieRestantDuJoueur>0);
-      }
+        } while (_pointDeVieRestantDeEnnemi > 0 || _pointDeVieRestantDuJoueur > 0);
+    }
 
 }
