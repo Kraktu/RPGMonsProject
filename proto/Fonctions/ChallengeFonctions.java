67dd68dc68b007f2aeee7006ac011d1bff522eaa
BrunoDefalque <defalquebruno@gmail.com>
2023-01-11 15:16:47 +0100

    Debut Challenge et test erreur

--- proto/Fonctions/ChallengeFonctions.java
+++ proto/Fonctions/ChallengeFonctions.java
@@ -1,20 +1,27 @@
 package proto.Fonctions;
-
+//Créé par bruno
 import proto.TousLesChallenges;
+import proto.lesPersonnagesEnCombat;
 
 public class ChallengeFonctions {
 
-    public void PrintDescriptionChallenge(){
-        System.out.println(
-        "voici le nom et la déscription d'un challenge :" +
-        "\n" +
-        "nom : " + TousLesChallenges.GetInstance().GetPontEnRuine().GetNom() +
-        "\n" +
-        "description : " + TousLesChallenges.GetInstance().GetPontEnRuine().GetDescription() +
-        "\n" +
-        "niveau : " + TousLesChallenges.GetInstance().GetPontEnRuine().GetNiveau()
-        );
+	private String tableauxChoixPossible = TousLesChallenges.GetInstance().GetPontEnRuine().GetChoixPossible()[0];
 
-    }
+	public void PrintDescriptionChallenge(lesPersonnagesEnCombat _lesPerso){
 
+	//	int _pointDeVieRestantDuJoueur = 0;
+
+	//	Joueur joueur = _lesPerso.GetLePersonnageJouable();
+
+		System.out.println("Parfait " + _lesPerso.GetLePersonnageJouable().GetNom() +
+		
+		"\nVous avez décidé de faire un challenge\n" +
+
+		"Pas de chance, c'est " + TousLesChallenges.GetInstance().GetPontEnRuine().GetNom() + " !!\n" +
+		
+		"Plusieurs choix s'offrent à vous ! Les voicis :\n" +
+
+		tableauxChoixPossible);
+
+	}
 }
