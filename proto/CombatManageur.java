e8b5e6acfe7a08a304bc84c6d9f15d5843021cb6
Sebastien Baloge <s.baloge@yahoo.fr>
2023-01-12 10:18:17 +0100

    correction des dégats

--- proto/CombatManageur.java
+++ proto/CombatManageur.java
@@ -17,13 +17,10 @@ public class CombatManageur {
     final MenuCombat _MenuCombat = new MenuCombat();
 
     public void SeBattre(lesPersonnagesEnCombat _lesPerso) {
-
         int _pointDeVieRestantDuJoueur = 0;
         int _pointDeVieRestantDeEnnemi = 0;
-
         Joueur joueur = _lesPerso.GetLePersonnageJouable();
         Ennemi ennemi = _lesPerso.GetEnnemisActuel();
-
         boolean leJoueurCommence = QuiCommence();
 
         do {
@@ -52,39 +49,10 @@ public class CombatManageur {
 
         } while (_pointDeVieRestantDeEnnemi > 0 && _pointDeVieRestantDuJoueur > 0);
 
-        /*
-         * do {
-         * 
-         * if (leJoueurCommence) {
-         * // Instruction pour le cas où le joueur commence.
-         * _pointDeVieRestantDeEnnemi = perso.GetEnnemisActuel().GetVie() -
-         * FormuleDegatPourEnnemi();
-         * System.out.println("Vous attaquez votre adversaire.\n Il lui reste " +
-         * _pointDeVieRestantDeEnnemi + ".");
-         * _pointDeVieRestantDuJoueur = perso.GetLePersonnageJouable().GetVie() -
-         * FormuleDegatPourJoueur();
-         * System.out.
-         * println("C'est au tour de votre ennemi de vous attaquer.\nIl vous rest " +
-         * _pointDeVieRestantDuJoueur);
-         * } else {
-         * // Instruction pour le cas où l'ennemi commence.
-         * _pointDeVieRestantDuJoueur = perso.GetLePersonnageJouable().GetVie() -
-         * FormuleDegatPourJoueur();
-         * System.out.println("Votre ennemi vous attaque.\nIl vous rest " +
-         * _pointDeVieRestantDuJoueur);
-         * _pointDeVieRestantDeEnnemi = perso.GetEnnemisActuel().GetVie() -
-         * FormuleDegatPourEnnemi();
-         * System.out.
-         * println("C'est à votre tour d'attaquer votre adversaire.\n Il lui reste " +
-         * _pointDeVieRestantDeEnnemi + ".");
-         * }
-         * 
-         * } while (_pointDeVieRestantDeEnnemi > 0 || _pointDeVieRestantDuJoueur > 0);
-         */
     }
 
     public int JetDeDe() {
-        int _jet = 0;
+            int _jet = 0;
         NbrAleatoire nbrAl = new NbrAleatoire();
         try {
             System.out.println("Le dé est lancé.");
@@ -96,18 +64,13 @@ public class CombatManageur {
         return _jet;
     }
 
-    public int FormuleDegatPourEnnemi() {
-        return perso.GetLePersonnageJouable().GetForce().GetValeur()
-                    - perso.GetEnnemisActuel().GetConstitution().GetValeur();
-    }
-
-    public int FormuleDegatPourJoueur() {
-        return perso.GetEnnemisActuel().GetForce().GetValeur()
-                - perso.GetLePersonnageJouable().GetConstitution().GetValeur();
-    }
-
     public int FormuleDegat(Personnage _attaquant, Personnage _defenseur) {
-        return _attaquant.GetForce().GetValeur() - _defenseur.GetConstitution().GetValeur();
+        int _degatsFinaux=_attaquant.GetForce().GetValeur() - _defenseur.GetConstitution().GetValeur();
+        if (_degatsFinaux==0) {
+_degatsFinaux=1;            
+        }
+        return _degatsFinaux;
+
     }
 
     public boolean QuiCommence() {
