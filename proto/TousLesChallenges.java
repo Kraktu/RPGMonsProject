da3d11a254e8de7be16427fd1b221b89d8290c8a
BrunoDefalque <defalquebruno@gmail.com>
2023-01-24 10:52:08 +0100

    Modif TousLesChallenges.java : Qlq print,, Stat dexterite, etc

--- proto/TousLesChallenges.java
+++ proto/TousLesChallenges.java
@@ -54,21 +54,21 @@ public final class TousLesChallenges {
 
 	// déclaration des tableaux de String :
 
-	String[] tempChoixPossible = {"Sautter au dessus", "Contourner le trou", "Ne pas prendre de risque"};
+	String[] tempChoixPossible = {"Sautter au dessus", "Escalader le mur à côté du trou", "Ne pas prendre de risque"};
 
-	String[] typeDeMalus = {"A : Si vous essayez de sauter au-dessus de celui-ci et que vous tombez dedans, vous perdrez ", "B : Si vous essayez de le contourner et que vous tombez dedans, vous perdrez ", "C : Si comme une poule mouillée, vous n'osez même pas jouer, vous perdrez "};
+	String[] typeDeMalus = {"A : Si vous essayez de sauter au-dessus de celui-ci et que vous tombez dedans, vous perdrez ", "B : Si vous essayez d'escalader le mur à côté du trou et que vous tombez dedans, vous perdrez ", "C : Si comme une poule mouillée, vous n'osez même pas jouer, vous perdrez "};
 
-	String[] typeDeRecompenses = {"A : Si magnifiquement vous sautez au-dessus du trou, vous gagnerez ", "B : Si vous contournez grâce a votre courage l'obstacle, vous gagnerez ", "C : Comme vous n'osez même pas jouer, vous gagnerez "};
+	String[] typeDeRecompenses = {"A : Si magnifiquement vous sautez au-dessus du trou, vous gagnerez ", "B : Par contre, plus difficile ! Si vous escaladez grâce a votre courage l'obstacle, vous gagnerez ", "C : Comme vous n'osez même pas jouer, vous gagnerez "};
 
 	// déclaration des tableaux int :
 
 	private int[] valeurMalus = {2, 2, 1};
 
-	int[] valeurRecompense = {4, 4, 0};
+	int[] valeurRecompense = {4, 8, 0};
 
 	Challenge pontEnRuine = new Challenge("Pont en ruine",
 
-	"C'est un pont en ruine! Très sinueux, un immense trou est présent et à cause de ceci on ne peut plus circuler normalement! Du coup…",
+	"Celui-ci est très sinueux, un immense trou est présent et à cause de ceci on ne peut plus circuler normalement! Du coup…",
 
 	"Par défaut",
 
@@ -84,7 +84,7 @@ public final class TousLesChallenges {
 
 	new StatistiqueBonus(3),// Stat Agilité
 
-	new StatistiqueBonus(1),// Stat Dextérité
+	new StatistiqueBonus(6),// Stat Dextérité
 
 	new StatistiqueBonus(0),// Stat Constitution
 
